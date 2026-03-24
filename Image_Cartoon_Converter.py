6ae43a2818ced6a5778b8a0f940b479d1e7c9489
Solaruse <kimsun1028@naver.com>
2026-03-24 21:43:50 +0900

    이미지 출력 변경

--- Image_Cartoon_Converter.py
+++ Image_Cartoon_Converter.py
@@ -1,26 +1,23 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 # 이미지 읽기
-img = cv2.imread('image.jpg')
+img = cv.imread('image.jpg')
 
 while img is not None:
     # 이미지 전처리
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.medianBlur(gray, 3)
-    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5,7)
-    color = cv2.bilateralFilter(img, 3, 500, 500)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray = cv.medianBlur(gray, 3)
+    edges = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 5,7)
+    color = cv.bilateralFilter(img, 3, 500, 500)
 
     # 두 이미지 합성
-    cartoon = cv2.bitwise_and(color, color, mask=edges)
-
-    # 원본 이미지와 붙이기
-    cartoon = np.hstack((img,cartoon))
+    cartoon = cv.bitwise_and(color, color, mask=edges)
 
     # 출력
-    cv2.imshow("Original | Cartoon Rendering", cartoon)
-    key = cv2.waitKey(0)
+    cv.imshow("Cartoon Rendering", cartoon)
+    key = cv.waitKey(0)
     if key == 27:
         break
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 
